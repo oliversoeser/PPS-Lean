cfb276e2240ad642b1fc36e53a49d750e37e76d6
Oliver Soeser <hello@oliversoeser.com>
2024-10-27 12:24:08 +0000

    Proof of Theorem 1.1

--- PPSLean/C1_Logic.lean
+++ PPSLean/C1_Logic.lean
@@ -1,19 +1,30 @@
 -- Week 1: Chapter 1, Logic
 
-import Mathlib.Data.Set.Basic
-
 variable (a b : Prop)
+
+#check Or.intro_left
+#check Or.intro_right
+#check Classical.byCases
+#check Or.elim
 
 -- Theorem 1.1
 theorem imp_iff_or : (a → b) ↔ (¬a ∨ b) :=
   Iff.intro
-    (fun (f : a → b) => sorry)
-    (fun (h : ¬a ∨ b) => sorry)
+    (fun (h₀ : a → b) => Classical.byCases
+      (fun (h₁ : a) => Or.inr (h₀ h₁))
+      (fun (h₂ : ¬a) => Or.inl h₂))
+    (fun (h₀ : ¬a ∨ b) => Or.elim h₀
+      (fun (h₁ : ¬a) => fun (h₂ : a) => absurd h₂ h₁)
+      (fun b => fun _ => b))
 
 -- Theorem 1.2
 theorem contrapos : (a → b) ↔ (¬b → ¬a) :=
-  sorry
+  Iff.intro
+    sorry
+    sorry
 
 -- Theorem 1.3
-theorem not_or_iff : ¬(a ∨ b) ↔ ¬a ∨ ¬b :=
-  sorry
+theorem not_or_iff : ¬(a ∨ b) ↔ ¬a ∧ ¬b :=
+  Iff.intro
+    sorry
+    sorry
